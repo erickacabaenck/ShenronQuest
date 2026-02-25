062cb07d69e873da5762489e3eadf390f5ad1082
rocket180406 <rocketdans18@gmail.com>
2026-02-25 14:26:02 +0100

    Revert "agua otaku"
    This reverts commit 337c9ced81bfd7e08a20f3399b0db8c23d140cec.

--- Shenron-quest/goku/player/player.gd
+++ Shenron-quest/goku/player/player.gd
@@ -9,15 +9,20 @@ extends CharacterBody2D
 @export var air_friction = 700
 
 @onready var ani_player = $AnimatedSprite2D
+## @onready var contador: Control = $CanvasLayer/Contador
 
+## var monedas: int = 0
 var atacar: bool = false
+
 
 func _ready() -> void:
 	add_to_group("jugadores")
+	## contador.actualizar(0)
+
 
 func apply_gravity(delta):
 	if not is_on_floor():
-		velocity.y += get_gravity().y * gravity_scale * delta
+		velocity+=get_gravity() * delta * gravity_scale
 		
 func apply_friction(input_axis, delta):
 	if input_axis==0 and is_on_floor():
@@ -65,31 +70,19 @@ func _physics_process(delta: float) -> void:
 	apply_friction(input_axis, delta)
 	handle_jump()
 	handle_air_acceleration(input_axis, delta)
-	
 	if Input.is_action_just_pressed("atacar") and not atacar:
 		ejecutar_ataque()
 	if not atacar:
 		update_animation(input_axis)
-		
 	move_and_slide()
 	
-	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(i)
-		if collision.get_collider().is_in_group("agua"):
-			morir()
 ## func add_moneda():
 	## monedas+=1
 	## contador.actualizar(monedas)
 	
 func morir():
-	if not is_physics_processing(): 
-		return
 	set_physics_process(false)
-	ani_player.play("dead")
-	if has_node("tiempo"):
-		$tiempo.start()
-		await $tiempo.timeout
+	ani_player.play("muerte")
 	$tiempo.start()
 	await $tiempo.timeout
-	if is_inside_tree():
-		get_tree().reload_current_scene()
+	get_tree().reload_current_scene()
