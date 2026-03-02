f6583093bc032766cd35c6e9864041e88d86e217
RODRIGAMER669 <alexis.rr0501@gmail.com>
2026-03-02 12:56:02 +0100

    contador no reinicia al morir

--- Shenron-quest/goku/menu_muerte/menu_muerte.gd
+++ Shenron-quest/goku/menu_muerte/menu_muerte.gd
@@ -1,8 +1,10 @@
 extends Control
 
 func _on_btn_restart_pressed():
+	Global.resetear_juego()
 	get_tree().change_scene_to_file("res://environment/environment.tscn")
 
 
 func _on_btn_menu_pressed():
+	Global.resetear_juego()
 	get_tree().change_scene_to_file("res://menu/menu.tscn")
