76850dc2db1553cfe565980ac8a8a5fe915be345
RODRIGAMER669 <alexis.rr0501@gmail.com>
2026-03-02 12:40:49 +0100

    contador de vida visual hecho

--- Shenron-quest/goku/contador_vidas/contador_vidas.gd
+++ Shenron-quest/goku/contador_vidas/contador_vidas.gd
@@ -3,11 +3,8 @@ extends Control
 @onready var label_vidas = $LabelVidas
 
 func _ready():
-	var jugador = get_tree().get_first_node_in_group("jugadores")
-	if jugador:
-		jugador.vidas_actualizadas.connect(actualizar_texto_vidas)
-	
 	actualizar_texto_vidas(Global.vidas)
 
 func actualizar_texto_vidas(cantidad):
-	label_vidas.text = "Vidas: " + str(cantidad)
+	if label_vidas: 
+		label_vidas.text = "Vidas: " + str(cantidad)
