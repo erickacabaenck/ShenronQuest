fccdb33c78241bf1a61c0d20344823054ddbdba1
rocket180406 <rocketdans18@gmail.com>
2026-03-02 20:56:39 +0100

    numero total de bolas actualizado

--- Shenron-quest/goku/contador_bolas/contador_bolas.gd
+++ Shenron-quest/goku/contador_bolas/contador_bolas.gd
@@ -5,8 +5,8 @@ extends Control
 func _process(_delta):
 	etiqueta.text = "Esferas: " + str(Global.bolas_recogidas) + "/7"
 	
-	if Global.bolas_recogidas >= 2:
+	if Global.bolas_recogidas >= 7:
 		cambiar_de_nivel()
 
 func cambiar_de_nivel():
-	get_tree().change_scene_to_file("res://menu/menu.tscn")
+	get_tree().change_scene_to_file("res://cinematica_final/cinematica_final.tscn")
